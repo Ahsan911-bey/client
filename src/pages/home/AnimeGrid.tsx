b10b7b931e0a1e323ec4c97abed7c3d72d2d7683
Ahsan911-bey <ahsanshahzadpakcelestial@gmail.com>
2025-01-20 15:39:29 +0500

    Added Responsivness according to 2xl

--- src/pages/home/AnimeGrid.tsx
+++ src/pages/home/AnimeGrid.tsx
@@ -56,10 +56,10 @@ const AnimeGrid: React.FC = () =>{
 
     return(
         <div className='flex gap-4 2xl:gap-7'>
-            <div className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5  gap-4'>
+            <div className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4'>
                 {AnimeList.map((anime) =>(
                     <div key={anime.id} className='group relative mt-4 ml-2 shadow-inner overflow-hidden transition ease-in-out hover:scale-105 duration-300'>
-                        <img src={anime.image} alt={anime.title} className=' object-cover rounded-lg 2xl:h-80 w-52' />
+                        <img src={anime.image} alt={anime.title} className=' object-cover rounded-lg 2xl:h-80 w-52 2xl:w-56' />
                         <div className='group-hover:opacity-60 absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-55'></div>
                             <div className='absolute bottom-0 left-2 drop-shadow-2xl -translate-y-1 p-2 '>
                                 <p className='text-white font-bold line-clamp-2'>{anime.title}</p>
